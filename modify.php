8fa8b33ed2a2fec1e0e46100cbad489e085d2ff7
Nyhx1213 <matis1013@gmail.com>
2025-03-13 17:47:32 +0100

    Made some SQL queries in modify.php, very minor changed in manageexistinggames.php .

--- modify.php
+++ modify.php
@@ -15,9 +15,22 @@ require_once "permission.inc.php";
     <body>
         <?php
             if (isset($_GET['idRencontre']) && !empty($_GET['idRencontre'])){
-                $sqlchart= 'SELECT Scoreteam1, Scoreteam2 FROM Rencontre
-                            WHERE RencontreID = :id';
-                            
+                $sqlchart = 'SELECT DISTINCT E1.NomEquipe, E2.NomEquipe FROM JOUER as J1
+                             INNER JOIN Equipe as E1 ON J1.EquipeID = E1.EquipeID
+                             INNER JOIN JOUER as J2 ON J1.RencontreID = J2.RencontreID 
+                             Inner JOIN Equipe as E2 ON J2.EquipeID = E2.EquipeID
+                             WHERE J1.RencontreID = 1 AND J1.EquipeID < J2.EquipeID';
+
+                $statementchart = $db->prepare($sqlchart);
+                $statementchart->bindParam(":id",$_GET['idRencontre']);
+
+                $sqlinsertrencontre = 'INSERT INTO Rencontre (Scoreteam1, Scoreteam2) Values(:score1, :score2)';
+ 
+                $sqlinsertjouer = 'INSERT INTO JOUER (Score' ; 
+
+                echo'
+                <form action="modify.php" method=post class="workout-form">
+                    <label for="scoreteam1"> ';
             }
         ?>
     </body>
