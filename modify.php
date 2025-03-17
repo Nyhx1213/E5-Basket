4c2a1ffd211383dd3dd06babc549692aa69cf106
Matis <matis1013@gmail.com>
2025-03-17 14:00:14 +0100

    modify.php now has a working SQL query + form is finished.

--- modify.php
+++ modify.php
@@ -13,24 +13,74 @@ require_once "permission.inc.php";
     </head>
 
     <body>
-        <?php
+        <header>
+            <nav>
+            <ul>
+                <li>
+                <?php if(isset($_SESSION['login'])){
+                        echo'<a href="profile.php">Welcome '.$_SESSION['login'].'</a>';
+                    }
+                ?>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <strong><a href="index.php">Basketball Management Application</a></strong>
+                </li>
+            </ul>
+            <ul>
+                <li> 
+                    <?php if(isset($_SESSION['login'])){
+                        echo'<a href="disconnect.php"> Disconnect</a>';
+                    }
+                        else echo '<a href="index.php">Login</a>';
+                    ?>
+                </li>
+            </ul>
+            </nav>
+        </header>
+        <main>
+
+            <?php
             if (isset($_GET['idRencontre']) && !empty($_GET['idRencontre'])){
-                $sqlchart = 'SELECT DISTINCT E1.NomEquipe, E2.NomEquipe FROM JOUER as J1
+                $sqlchart = 'SELECT E1.NomEquipe as Equipe1, E2.NomEquipe as Equipe2 FROM JOUER as J1
                              INNER JOIN Equipe as E1 ON J1.EquipeID = E1.EquipeID
                              INNER JOIN JOUER as J2 ON J1.RencontreID = J2.RencontreID 
                              Inner JOIN Equipe as E2 ON J2.EquipeID = E2.EquipeID
-                             WHERE J1.RencontreID = 1 AND J1.EquipeID < J2.EquipeID';
+                             WHERE J1.RencontreID = :id AND J1.EquipeID < J2.EquipeID';
 
                 $statementchart = $db->prepare($sqlchart);
                 $statementchart->bindParam(":id",$_GET['idRencontre']);
-
+                $statementchart->execute();
+                
                 $sqlinsertrencontre = 'INSERT INTO Rencontre (Scoreteam1, Scoreteam2) Values(:score1, :score2)';
- 
+                
                 $sqlinsertjouer = 'INSERT INTO JOUER (Score' ; 
+                
+                $row = $statementchart->fetch();
 
                 echo'
                 <form action="modify.php" method=post class="workout-form">
-                    <label for="scoreteam1"> ';
-            }
-        ?>
+                <select id="team1" name="team1" required>
+                        <option value="'.$row['Equipe1'].'">'.$row['Equipe1'].'</option>
+                    </select>
+
+                    <label for="scoreteam1"> Insert Score </label>
+                    <input type="number" id="scoreteam1" name="scoreteam1">
+                    <select id="team2" name="team2" required>
+                        <option value="'.$row['Equipe2'].'">'.$row['Equipe2'].'</option>
+                        </select>
+                        
+                        <label for="scoreteam2"> Insert Score </label>
+                    <input type="number" id="scoreteam2" name="scoreteam2">
+                    <input type="submit" value="Submit">
+                </form>';
+
+                if (isset($_POST['submit'])){
+                    
+                }
+                }
+            ?>
+        </main>
     </body>
+</html>
